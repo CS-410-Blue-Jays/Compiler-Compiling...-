cf7b06988f8920c95f236abdf6e58f73b0d112c0
pupilliluke <djc6421@psu.edu>
2024-12-18 14:41:25 -0500

    trying out user input

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,23 @@
 public class Main {
   public static void main(String[] args){
 
+	// System.out.println("");
+
+	// java.util.Scanner s = new java.util.Scanner(System.in);	//for prompting
+        
+	// System.out.print("Welcome: Execute Frontend? (Y or N):");	//prompt
+
+	// String result = s.nextLine();  // Reads the entire line (string input)
+	
+	
+
+
 	Frontend frontend = new Frontend();
 	String atom_output_fileName = frontend.executeFrontend();
 	
 	Backend backend = new Backend(atom_output_fileName);
 	backend.main(args);
 
+	// s.close();
 	} // End of main
 } // End of class
